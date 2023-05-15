1b9c5b72e126dc1a2d478b054f36b3be0bb01a3d
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-15 21:03:24 +0545

    get user and signup

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,22 +1,100 @@
 package controllers
 
+// imports
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/thaparoyal/golang-jwt/database"
+	helpers "github.com/thaparoyal/golang-jwt/helpers"
+	models "github.com/thaparoyal/golang-jwt/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-var userCollecion *mongo.Collection = database.OpenCollection(database.Client, "User")
+// Create a global variable to hold the MongoDB collection
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "User")
 var validate = validator.New()
 
 func Hashpassword() {}
 
 func VerifyPassword() {}
 
-func Signup() {}
+// Signup returns a Gin handler function for user signup.
+func Signup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		// Bind the JSON data from the request body to the user struct
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Validate the user struct using the validator
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		// Check if the email already exists in the collection
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking error"})
+		}
+
+		// Check if the phone number already exists in the collection
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking phone"})
+		}
+
+		// If either email or phone number already exists, return an error
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email or phone number already exists"})
+		}
+
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+	}
+}
 
 func Login() {}
 
 func GetUsers() {}
 
-func GetUser()
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Params("user_id")
+
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollecion.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,1 +1,39 @@
 package helpers
+
+// imports
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CheckUserType checks if the user type stored in the context matches the specified role.
+func CheckUserType(c *gin.Context, role string) (err error) {
+	// Retrieve the user type from the context
+	userType := c.GetString("user_type")
+	err = nil // Initialize the error variable as nil
+
+	// Compare the user type with the specified role
+	if userType != role {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	return err
+}
+
+// MatchUserTypeToUid checks if the user type and user ID stored in the context match the specified values.
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	// Retrieve the user type and user ID from the context
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil // Initialize the error variable as nil
+
+	// If the user type is "USER" and the user ID doesn't match the specified value, return an error
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	// Check if the user type matches the specified value by calling the CheckUserType function
+	err = CheckUserType(c, userType)
+	return err
+}
