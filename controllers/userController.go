fa70d69d80765890eb33bb7b4de7ae4aa505a7b3
thaparoyal17@gmail.com <thaparoyal17@gmail.com>
2023-05-16 00:46:53 +0545

    login func

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,8 @@
 package controllers
 
-// imports
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -15,64 +15,107 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// Create a global variable to hold the MongoDB collection
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "User")
 var validate = validator.New()
 
 func Hashpassword() {}
 
-func VerifyPassword() {}
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
 
-// Signup returns a Gin handler function for user signup.
+	if err != nil {
+		msg = fmt.Sprintf("email or password is incorrect")
+		check = false
+	}
+
+	return check, msg
+}
+
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
 		var user models.User
 
-		// Bind the JSON data from the request body to the user struct
 		if err := c.BindJSON(&user); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		// Validate the user struct using the validator
 		validationErr := validate.Struct(user)
 		if validationErr != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
 			return
 		}
 
-		// Check if the email already exists in the collection
 		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
 		defer cancel()
-
 		if err != nil {
 			log.Panic(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking error"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking email"})
 		}
 
-		// Check if the phone number already exists in the collection
-		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Phone})
+		count, err = userCollection.CountDocuments(ctx, bson.M{"email": user.Phone})
 		defer cancel()
 		if err != nil {
 			log.Panic(err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking phone"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while checking phone number"})
 		}
 
-		// If either email or phone number already exists, return an error
 		if count > 0 {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email or phone number already exists"})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "This email or phone number already exists"})
 		}
 
 		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
 		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
 		user.ID = primitive.NewObjectID()
+
+		user.User_id = user.ID.Hex()
+
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+
+		resultInsertionNumber, insertError := userCollection.InsertOne(ctx, user)
+		if insertError != nil {
+			msg := fmt.Sprintf("User item was not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, resultInsertionNumber)
 	}
 }
 
-func Login() {}
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Email or  password is incorrect"})
+			return
+		}
+		passwordIsValid, msg := VerifyPassword(*user.Password, *&foundUser.Password)
+		defer cancel()
+	}
+}
 
 func GetUsers() {}
 
@@ -84,13 +127,10 @@ func GetUser() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-
 		var user models.User
-		err := userCollecion.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
 		defer cancel()
-
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,19 +1,16 @@
 package helpers
 
-// imports
 import (
 	"errors"
 
 	"github.com/gin-gonic/gin"
 )
 
-// CheckUserType checks if the user type stored in the context matches the specified role.
 func CheckUserType(c *gin.Context, role string) (err error) {
-	// Retrieve the user type from the context
-	userType := c.GetString("user_type")
-	err = nil // Initialize the error variable as nil
 
-	// Compare the user type with the specified role
+	userType := c.GetString("user_type")
+	err = nil
+
 	if userType != role {
 		err = errors.New("Unauthorized to access this resource")
 		return err
@@ -21,19 +18,15 @@ func CheckUserType(c *gin.Context, role string) (err error) {
 	return err
 }
 
-// MatchUserTypeToUid checks if the user type and user ID stored in the context match the specified values.
 func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
-	// Retrieve the user type and user ID from the context
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
-	err = nil // Initialize the error variable as nil
+	err = nil
 
-	// If the user type is "USER" and the user ID doesn't match the specified value, return an error
 	if userType == "USER" && uid != userId {
 		err = errors.New("Unauthorized to access this resource")
 		return err
 	}
-	// Check if the user type matches the specified value by calling the CheckUserType function
 	err = CheckUserType(c, userType)
 	return err
 }
